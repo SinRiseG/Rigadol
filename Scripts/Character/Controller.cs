0992734a710680293e84136268a9719343049e9b
SinRiseG <pavel.burkov.github@mail.ru>
2021-05-02 14:02:48 +0300

    Update Controller.cs

--- Scripts/Character/Controller.cs
+++ Scripts/Character/Controller.cs
@@ -2,26 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace SinRise
+
+public class Controller : MonoBehaviour
 {
-	public class Controller : MonoBehaviour
+	public BoxCollider boxCollider;
+	public CharacterInput characterInput;
+	public CharacterAnimation characterAnimation;
+	public CharacterMovement characterMovement;
+
+
+	void Update ()
 	{
-		public BoxCollider boxCollider;
-		public CharacterInput characterInput;
-		public CharacterAnimation characterAnimation;
-		public CharacterMovement characterMovement;
-
-		void Start ()
-		{
-			boxCollider = AddComponentMenu (BoxCollider);
-		}
-
-
-		void Update ()
-		{
-			characterInput.InputUpdate ();
-			characterAnimation.AnimationUpdate ();
-			characterMovement.MoveUpdate ();
-		}
+		characterInput.InputUpdate ();
+		characterAnimation.AnimationUpdate ();
+		characterMovement.MoveUpdate ();
 	}
 }
